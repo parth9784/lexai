180254a654e8205ad8fdb84d77614c4daf244f60
parth9784 <pdsharma458@gmail.com>
2025-07-12 23:18:52 +0530

    improved

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar({ }: any) {
                     ))}
                     <li>
                         <button className="w-full bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-200">
-                            Try Demo
+                            Login
                         </button>
                     </li>
                 </ul>
